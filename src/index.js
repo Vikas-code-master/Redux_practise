2555d409e3eb568f7a88d786f307810de5731519
vikas-code-master <vikas201095r@gmail.com>
2021-06-28 11:36:54 +0530

    add Test files

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './Store/index'
-
+import Test from './components/Test'
 ReactDOM.render(
 
 <Provider store ={store} >
     <App />
+    <Test/>
 </Provider>
 
 , document.getElementById('root'));
